52dfdc806035f60b1380baaa703601fc72beae2f
Marcos Vnt <marcos.visualnt@gmail.com>
2020-01-03 01:07:27 +0100

    bootstrap-vue install

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
+
 
 /**
  * The following block of code may be used to automatically register your
