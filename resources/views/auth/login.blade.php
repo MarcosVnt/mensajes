af14786a24cd5a55ced5a6afe6e9c988fb6ba9d0
Marcos Vnt <marcos.visualnt@gmail.com>
2020-01-03 01:29:22 +0100

    login.blade a componente vue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<b-container>
+    <b-row align-h="center">
+        <b-col cols="8">
 
-                <div class="card-body">
-                        <b-alert variant="success" show>Success Alert</b-alert>
+            <b-card title="Inicio de sesión" class="my-3">
+                
+              
+             
+                    <b-alert show>
+                        Por favor ingresa tus datos:
+                    </b-alert>
+             
+                <b-form method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
 
-                        
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <b-form-group
+                        label="Correo electrónico" label-for="email">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <b-form-input type="email"                            
+                            id="email" name="email"
+                            value="{{ old('email') }}" required autofocus
+                            placeholder="example@programacionymas.com">
+                        </b-form-input>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    </b-form-group>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                    <b-form-group label="Contraseña" label-for="password">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <b-form-input type="password" 
+                            id="password" name="password" required>
+                        </b-form-input>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    </b-form-group>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                    <b-form-group>
+                        <b-form-checkbox name="remember" 
+                            {{ old('remember') ? 'checked="true"' : '' }}>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            Recordar sesión
+                        </b-form-checkbox>
+                    </b-form-group>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                    <b-button type="submit" variant="primary">
+                        Ingresar
+                    </b-button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                    <b-button href="{{ route('password.request') }}" variant="link">
+                        ¿Olvidaste tu contraseña?
+                    </b-button>
+
+                </b-form>
+
+            </b-card>
+
+
+        </b-col>
+    </b-row>
+</b-container>
 @endsection
